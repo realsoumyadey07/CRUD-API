cdf0c546130f298a237e34eb20d1db87f11385fb
realsoumyadey07 <soumyadipdey802@gmail.com>
2024-02-26 17:29:59 +0530

    CRUD API is completed

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = 8000;
 
 //middleware
 app.use(express.json());
-
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res)=>{
      res.send("Welcome to the server!");
@@ -59,8 +59,17 @@ app.put('/api/product/:id', async (req, res)=>{
 });
 
 //delete product
-app.delete('', async (req, res)=>{
-
+app.delete('/api/product/:id', async (req, res)=>{
+     try {
+          const {id} = req.params;
+          const product = await Product.findByIdAndDelete(id);
+          if(!product){
+               return res.status(404).json({message: "Product not found"});
+          }
+          res.status(200).json({message: "Product deleted successfully"});
+     } catch (error) {
+          res.status(500).json({message: error.message});
+     }
 })
 
 
